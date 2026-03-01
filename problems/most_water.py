9bd070fa32cf23aed9b70a6f0ee07def0ec20693
Gowtham <jayavarapugowtham@gmail.com>
2026-03-02 01:49:20 +0530

    one of the most intresting question,time comp is n square i need to make it n tomm i will take care of it

---
+++ problems/most_water.py
@@ -0,0 +1,27 @@
+# 10. Container With Most Water
+# You are given an integer array height of length n. There are n
+# vertical lines drawn such that the two endpoints of the ith line are
+# (i, O) and (i, height[I]).
+
+# Find two lines that together with the x-axis form a container, such
+# that the container contains the most water.
+# Return the maximum amount of water a container can store.
+
+# Input: height = [1,8,6,2,5,4,8,3,7]
+# Output: 49
+# Explanation: The above vertical lines are represented by an array
+# [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section)
+# the container can contain is 49.
+
+height = [1,8,6,2,5,4,8,3,7]
+def most_water(arr):
+    Area = []
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            height = min(arr[i],arr[j])
+            width = j-i
+            area = height*width
+            Area.append(area)
+    return max(Area)
+
+print(most_water(height))
